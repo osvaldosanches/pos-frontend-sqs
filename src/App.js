9bbd2e875bfa1aeb692906255536a46cf7b89adb
osvaldosanches <vado.benites@gmail.com>
2021-02-06 22:31:23 -0300

    comentários

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
+//Osvaldo Sanches
+//Projeto - POC PUC Minas
+//2020-2021
+
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import Axios from "axios";
 
 function App() {
 
-
   const [nome, setNome] = useState("");
   const [descricao, setDescricao] = useState("");
   const [normaList, setNormaList] = useState([]);
 
-  const [novaDescricao, setNovaDescricao] = useState("");
-
+  //chamada de busca
   useEffect(()=>{
     Axios.get("https://pos-backend-sqs-2021.herokuapp.com/api/get").then((response) => {
 
       setNormaList(response.data);
-      //alert(response);
-      //alert(response.data);
-      //alert(response.data.ReceiveMessageResponse.ReceiveMessageResult.messages[0].Body);
 
     });
   },[]);
 
+  // chamada para inserir
   const enviar = () =>{
-
 
     Axios.post("https://pos-backend-sqs-2021.herokuapp.com/api/insert",{
       nome:nome, 
